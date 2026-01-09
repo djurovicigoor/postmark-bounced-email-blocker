ddee65dc3d24cf2b28ae2b6d3033bd45ca9e599f
Djurovic Igor <djurovicigoor@users.noreply.github.com>
2026-01-09 10:38:06 +0100

    Add early return for empty email value in validation

--- src/Validation/BouncedEmailInPostmark.php
+++ src/Validation/BouncedEmailInPostmark.php
@@ -27,6 +27,10 @@ class BouncedEmailInPostmark
      */
     public function validate($attribute, $value, $parameters, $validator): bool
     {
+        if ( ! $value) {
+            return true;
+        }
+        
         $hasValidateDomain = in_array('validate_domain', $parameters, true);
         if ($hasValidateDomain) {
             return PostmarkBouncedEmailBlockerFacade::enableDomainBlocking()->isNotBlocked($value);
