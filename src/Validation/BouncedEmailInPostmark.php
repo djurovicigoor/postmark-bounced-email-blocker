3b3d25c85d6a9d922fa350f81e2dde9785ce4589
Djurovic Igor <djurovicigoor@users.noreply.github.com>
2026-01-09 07:54:15 +0100

    Enhance email validation with domain check option

--- src/Validation/BouncedEmailInPostmark.php
+++ src/Validation/BouncedEmailInPostmark.php
@@ -2,30 +2,35 @@
 
 namespace Djurovicigoor\PostmarkBouncedEmailBlocker\Validation;
 
-use Djurovicigoor\PostmarkBouncedEmailBlocker\Facades\PostmarkBouncedEmailBlockerFacade;
 use Illuminate\Validation\Validator;
+use Djurovicigoor\PostmarkBouncedEmailBlocker\Facades\PostmarkBouncedEmailBlockerFacade;
 
-class BouncedEmailInPostmark {
-	
-	/**
-	 * Default error message.
-	 *
-	 * @var string
-	 */
-	public static string $errorMessage = 'It\'s not possible to send email to this address because the recipient has flagged your previous email as spam.';
-	
-	/**
-	 * Validates whether an email address does not list in Postmark bounced emails list.
-	 *
-	 * @param string    $attribute
-	 * @param mixed     $value
-	 * @param array     $parameters
-	 * @param Validator $validator
-	 *
-	 * @return bool
-	 */
-	public function validate($attribute, $value, $parameters, $validator): bool {
-		
-		return PostmarkBouncedEmailBlockerFacade::isNotBlocked($value);
-	}
+class BouncedEmailInPostmark
+{
+    
+    /**
+     * Default error message.
+     *
+     * @var string
+     */
+    public static string $errorMessage = 'It\'s not possible to send email to this address because the recipient has flagged your previous email as spam.';
+    
+    /**
+     * Validates whether an email address does not list in Postmark bounced emails list.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  array  $parameters
+     * @param  Validator  $validator
+     *
+     * @return bool
+     */
+    public function validate($attribute, $value, $parameters, $validator): bool
+    {
+        $hasValidateDomain = in_array('validate_domain', $parameters, true);
+        if ($hasValidateDomain) {
+            return PostmarkBouncedEmailBlockerFacade::enableDomainBlocking()->isNotBlocked($value);
+        }
+        return PostmarkBouncedEmailBlockerFacade::isNotBlocked($value);
+    }
 }
